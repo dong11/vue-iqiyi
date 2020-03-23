e23e7fddac7b417e021ea8f85ea4aa1bf81f5a0d
hwongrex <hwongrex@gmail.com>
2020-03-23 22:06:13 +0800

    svg

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <i-header></i-header>
   </div>
 </template>
 
 <script>
+import IHeader from '@/components/IHeader'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'i-header': IHeader
+  }
 }
 </script>
 
@@ -18,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/IHeader/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <header class="i-header">
+    <div class="header-inner">
+      <svg-icon icon-class="logo-iqiyi" class-name="qy-logo"></svg-icon>
+
+      <div class="qy-search">
+        <div class="search-box">
+          <span class="search-box-in">
+            <input class="search-input">
+            <i>热搜榜</i>
+          </span>
+          <span class="search-box-out"></span>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.qy-logo {
+  float: left;
+  width: 120px !important;
+  height: 40px !important;
+  fill: #00be06 !important;
+}
+</style>
+
+<style lang="scss" scoped>
+.i-header {
+  width: 100%;
+  height: 64px;
+  background-color: #232325;
+
+  .header-inner {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .qy-search {
+      width: 360px;
+      height: 42px;
+      position: relative;
+      display: flex;
+      align-items: center;
+
+      .search-box {
+        height: 100%;
+
+        .search-box-in {
+          position: relative;
+          width: 230px;
+
+          .search-input {
+
+          }
+
+          .search-hot {
+            position: absolute;
+            right: 60px;
+          }
+        }
+
+        .search-box-out {
+
+        }
+      }
+    }
+  }
+}
+</style>
