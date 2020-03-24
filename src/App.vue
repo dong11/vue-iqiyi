baf32c084647ef43efa32dbe2483bce2f0fccfca
hwongrex <hwongrex@gmail.com>
2020-03-24 23:37:04 +0800

    搜索框

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,44 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+@font-face {
+  font-family: iconfont;
+  src: url(https://static.qiyi.com/ext/common/pcw-v4-font/iconfont.eot);
+  src: url(https://static.qiyi.com/ext/common/pcw-v4-font/iconfont.eot#iefix)
+      format("embedded-opentype"),
+    url(https://static.qiyi.com/ext/common/pcw-v4-font/iconfont.woff)
+      format("woff"),
+    url(https://static.qiyi.com/ext/common/pcw-v4-font/iconfont.ttf)
+      format("truetype"),
+    url(https://static.qiyi.com/ext/common/pcw-v4-font/iconfont.svg#iconfont)
+      format("svg");
+}
+
+.qy-svgicon {
+  vertical-align: -1px;
+  font-family: iconfont !important;
+  font-size: 16px;
+  font-style: normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+}
+
+.qy-svgicon-search:before {
+  content: "\E866";
+}
+
+.qy-svgicon-rank-hot2:before {
+  content: "\EA7C";
+}
+
+.qy-svgicon-trashcan:before {
+    content: "\E867"
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
 </style>
--- src/components/IHeader/index.vue
+++ src/components/IHeader/index.vue
@@ -3,13 +3,64 @@
     <div class="header-inner">
       <svg-icon icon-class="logo-iqiyi" class-name="qy-logo"></svg-icon>
 
+      <!-- 搜索框 -->
       <div class="qy-search">
         <div class="search-box">
+          <!-- 搜索输入框 -->
           <span class="search-box-in">
-            <input class="search-input">
-            <i>热搜榜</i>
+            <input
+              class="search-input"
+              placeholder="王牌对王牌第5季2020"
+              @focus="onShowSearchList(true)"
+              @blur="onShowSearchList(false)"
+            />
+            <a class="search-hot">
+              <i class="qy-svgicon qy-svgicon-rank-hot2"></i>
+              热搜榜
+            </a>
           </span>
-          <span class="search-box-out"></span>
+          <!-- 搜索按钮 -->
+          <span class="search-box-out">
+            <span class="search-box-btn">
+              <i class="qy-svgicon qy-svgicon-search"></i>
+            </span>
+          </span>
+        </div>
+        <!-- 搜索列表框 -->
+        <div v-show="showSearchResult" class="search-result">
+          <div class="search-result-con" @mouseenter="onMouseInBox(true)" @mouseleave="onMouseInBox(false)">
+            <!-- 历史搜索记录 -->
+            <div v-if="searchHistroyList.length > 0" class="search-result-history">
+              <div class="search-result-title">
+                历史搜索
+                <span class="search-result-clear" @click="clearSearchHistory()">
+                  <i class="qy-svgicon qy-svgicon-trashcan"></i>
+                  清除记录
+                </span>
+              </div>
+              <a
+                v-for="item in searchHistroyList"
+                :key="item"
+                href="javascript:void(0);"
+                class="search-result-item"
+              >
+                <div class="search-result-simple">
+                  <span class="search-result-text">{{item}}</span>
+                </div>
+              </a>
+            </div>
+
+            <!-- 热门搜索 -->
+            <div class="search-result-hot">
+              <div class="search-result-title">热门搜索</div>
+              <a v-for="(item, index) in searchHotList" :key="item" href="javascript:void(0);" class="search-result-item">
+                <div class="search-result-simple">
+                  <em :class="`search-result-num search-result-num${index + 1}`">{{index + 1}}</em>
+                  <span class="search-result-text">{{item}}</span>
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -18,7 +69,37 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      showSearchResult: false,
+      mouseInBox: false, // 鼠标是否在搜索列表框内，用于判断是否可隐藏搜索列表
+      searchHistroyList: ['王牌对王牌'],
+      searchHotList: [
+        '青春有你',
+        '快进者',
+        '王牌对王牌',
+        '奥特曼',
+        '冰糖炖雪梨',
+        '小猪佩奇',
+        '爱情公寓',
+        '无心法师',
+        '安家'
+      ]
+    }
+  },
+  methods: {
+    onShowSearchList (show) {
+      !this.mouseInBox && (this.showSearchResult = show)
+    },
+    onMouseInBox (inBox) {
+      console.log('====onMouseInBox====', inBox)
+      this.mouseInBox = inBox
+    },
+    clearSearchHistory () {
+      console.log('===clearSearchHistory===')
+      this.searchHistroyList = []
+    }
+  }
 }
 </script>
 
@@ -44,37 +125,192 @@ export default {
     margin: 0 auto;
     display: flex;
     align-items: center;
-    justify-content: center;
+    box-sizing: border-box;
+    padding: 0 87px;
 
     .qy-search {
       width: 360px;
       height: 42px;
-      position: relative;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
       display: flex;
       align-items: center;
 
       .search-box {
         height: 100%;
+        width: 100%;
+        background-color: #333;
+        border-radius: 21px;
+
+        &:hover {
+          background-color: #444;
+          transition: background-color 0.2s;
+        }
 
         .search-box-in {
           position: relative;
           width: 230px;
+          display: block;
+          letter-spacing: 0;
+          padding-right: 80px;
 
           .search-input {
-
+            width: 100%;
+            padding: 11px 0;
+            background-color: transparent;
+            border: 0;
+            outline: 0;
+            text-indent: 17px;
+            border-radius: 21px 0 0 21px;
+            height: 20px;
+            line-height: 20px;
+            font-size: 16px;
+            color: #999;
+            font-family: Microsoft Yahei;
           }
 
           .search-hot {
             position: absolute;
-            right: 60px;
+            top: 50%;
+            right: 10px;
+            margin-top: -10px;
+            color: #666;
+            line-height: 20px;
+            height: 20px;
+            font-size: 14px;
+
+            &:hover {
+              color: #00be06;
+              transition: color 0.2s;
+            }
           }
         }
 
         .search-box-out {
+          position: absolute;
+          top: 0;
+          right: 0;
 
+          .search-box-btn {
+            display: block;
+            width: 50px;
+            height: 18px;
+            padding: 12px 0;
+            line-height: 18px;
+            background-color: #00be06;
+            border-radius: 0 21px 21px 0;
+            color: #fff;
+            text-align: center;
+            font-size: 16px;
+            cursor: pointer;
+          }
+        }
+      }
+
+      .search-result {
+        position: absolute;
+        width: 291px;
+        left: 19px;
+        right: 96px;
+        top: 42px;
+        z-index: 1001;
+
+        .search-result-con {
+          padding: 3px 0 6px;
+          border: 1px solid #f0f0f0;
+          border-top: 0;
+          box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+          background-color: #fff;
+
+          .search-result-title {
+            height: 32px;
+            padding: 0 10px 0 15px;
+            line-height: 32px;
+            font-size: 14px;
+            color: #999;
+          }
+
+          .search-result-clear {
+            float: right;
+            color: #00be06;
+            cursor: pointer;
+          }
+
+          .search-result-simple {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
+
+          .search-result-item {
+            display: block;
+            height: 32px;
+            padding: 0 10px 0 15px;
+            line-height: 32px;
+            font-size: 14px;
+            color: #222;
+            cursor: pointer;
+
+            &:hover {
+              color: #00be06;
+              background-color: #f5f5f5;
+            }
+          }
+
+          .search-result-num {
+              display: inline-block;
+              width: 23px;
+              height: 18px;
+              margin-right: 10px;
+              line-height: 18px;
+              color: #fff;
+              border-radius: 0 9px 9px 0;
+              background-color: #d8d8d8;
+              text-align: center;
+              &.search-result-num1 {
+                background-color: #00be06;
+              }
+              &.search-result-num2 {
+                background-color: #4cd150;
+              }
+              &.search-result-num3 {
+                background-color: #99e59b;
+              }
+          }
         }
       }
     }
+
+    .header-left {
+      position: absolute;
+      right: 0;
+    }
   }
+}
+
+input {
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  color: -internal-light-dark-color(black, white);
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+  -webkit-appearance: textfield;
+  background-color: -internal-light-dark-color(white, black);
+  -webkit-rtl-ordering: logical;
+  cursor: text;
+  margin: 0em;
+  font: 400 11px system-ui;
+  padding: 1px;
+  border-width: 2px;
+  border-style: inset;
+  border-color: initial;
+  border-image: initial;
 }
 </style>
