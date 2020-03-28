00e2f11b62bfd930a9beee48f020f7cab59de746
hwongrex <hwongrex@gmail.com>
2020-03-28 23:54:09 +0800

    适配方案: flexible+px2remLoader

--- build/utils.js
+++ build/utils.js
@@ -29,9 +29,17 @@ exports.cssLoaders = function (options) {
     }
   }
 
+  const px2remLoader = {
+    loader: 'px2rem-loader',
+    options: {
+      remUnit: 192,  //根据视觉稿，rem为px的十分之一，1920px  192 rem
+      // remPrecision: 8//换算的rem保留几位小数点
+    }
+  }
+
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader, px2remLoader] : [cssLoader, px2remLoader]
 
     if (loader) {
       loaders.push({
